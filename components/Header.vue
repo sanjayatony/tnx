e3f820ff2f11859fd465f3eb6ef056fd4231cc1b
Tony S <sanjayatony@gmail.com>
2020-07-06 15:10:57 +0700

    add header component

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="header max-w-xl mx-auto p-4">
+      <h1 class="text-4xl"><a href="/">tonys.</a></h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
