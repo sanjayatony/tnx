4b18f4ed5f2f743dcd91ddb22b38190486921f2a
Tony S <sanjayatony@gmail.com>
2020-07-06 15:19:15 +0700

    tes nuxt-link

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="max-w-xl p-4">
+    <p class="text-xl">About</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore nam illum, aliquam, blanditiis cupiditate nemo velit ullam praesentium voluptas molestias et ducimus, voluptatum corporis minus aperiam hic harum natus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore nam illum, aliquam, blanditiis cupiditate nemo velit ullam praesentium voluptas molestias et ducimus, voluptatum corporis minus aperiam hic harum natus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore nam illum, aliquam, blanditiis cupiditate nemo velit ullam praesentium voluptas molestias et ducimus, voluptatum corporis minus aperiam hic harum natus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tempore nam illum, aliquam, blanditiis cupiditate nemo velit ullam praesentium voluptas molestias et ducimus, voluptatum corporis minus aperiam hic harum natus?</p>
+  </div>
+</template>
