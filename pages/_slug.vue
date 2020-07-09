d02872f6ada38cc2189ae111af09c4d9d2278fc5
Tony S <sanjayatony@gmail.com>
2020-07-09 10:03:28 +0700

    this will handle pages md

---
+++ pages/_slug.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="max-w-2xl mx-auto p-8">
+      <nuxt-content :document="doc" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const doc = await $content(params.slug || "index").fetch();
+    return { doc };
+  },
+  head() {
+    return {
+      title: this.doc.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.doc.description
+        }
+      ]
+    };
+  }
+};
+</script>
