fc9c3814d1500f2b666062c55dfef85b7127688d
Tony S <sanjayatony@gmail.com>
2020-07-09 17:02:53 +0700

    update now

--- now.js
+++ now.js
@@ -1,21 +1,10 @@
 {
   "version": 2,
-  "env": {
-      "ON_VERCEL": "true"
-  },
   "builds": [
-      {
-          "src": "api/**/*.js",
-          "use": "@now/node"
-      },
-      {
-          "src": "nuxt.config.js",
-          "use": "@nuxtjs/now-builder"
-      }
-  ],
-  "routes": [
-      {  "src": "/api/(.*)", "dest": "api/index.js" },
-      { "src": "/api", "dest": "api/index.js" },
-      { "src": "/(.*)", "dest": "$1" }
+    {
+      "src": "nuxt.config.js",
+      "use": "@nuxtjs/now-builder",
+      "config": {}
+    }
   ]
 }
