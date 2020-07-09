40714b71c50a78aa4548cc188a4681835aba6643
Tony S <sanjayatony@gmail.com>
2020-07-09 11:03:28 +0700

    add footer comp

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="footer max-w-2xl mx-auto p-4 mt-8 text-center">
+      <p class="text-base">
+        Mention me at
+        <a href="https://twitter.com/sanjayatony" target="_blank"
+          >@sanjayatony</a
+        >, or email
+        <a href="mailto:hi@tonysanjaya.me" target="_blank">hi@sanjayatony</a>.
+      </p>
+    </div>
+  </div>
+</template>
