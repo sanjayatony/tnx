2abcf84592a8d19ba1db46ca7838ec67b7f9d039
Tony S <sanjayatony@gmail.com>
2020-07-12 01:23:48 +0700

    add robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content", "@nuxtjs/robots"],
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
