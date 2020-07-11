b70a5acd2164a4061db59ac04c8397c0e3034e3d
Tony S <sanjayatony@gmail.com>
2020-07-11 15:56:40 +0700

    add devdad page

---
+++ pages/devdad.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Under construction 🚧</p>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "Devdad | Tony Sanjaya",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Devdad"
+        }
+      ]
+    };
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
     <p>Since you already here, I will introduce myself.</p>
 
     <p>
-      I am Tony, a web developer and a dad (devdad) from Indonesia. I have been
+      I am Tony, a web developer and a dad (
+      <nlink to="/devdad">devdad</nlink>) from Indonesia. I have been
       a professional web developer since 2012.
     </p>
 
