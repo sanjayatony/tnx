3061a01cfdcb53980fbd0a51374378c383db0651
Tony S <sanjayatony@gmail.com>
2020-07-11 14:13:33 +0700

    add ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-mode
    */
   mode: "spa",
+  ssr: "true",
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
--- pages/README.md
+++
@@ -1,1 +0,0 @@
-This is for my personal homepage https://tonysanjaya.me. Using Nuxt Content
