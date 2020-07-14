04c319b89e580ba11cbba6abbad9ad6ee4ccb9fc
Tony S <sanjayatony@gmail.com>
2020-07-14 14:25:13 +0700

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/png", href: "/geek.png" }]
   },
   /*
    ** Global CSS
