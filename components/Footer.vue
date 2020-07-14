82337f717fcc7cbec08379e2836c0df8c20b8031
Tony S <sanjayatony@gmail.com>
2020-07-14 12:14:42 +0700

    add some text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <div class="footer max-w-2xl mx-auto p-4 mt-8 text-center">
-      <p class="text-base">
-        Mention me at
-        <a href="https://twitter.com/sanjayatony" target="_blank"
-          >@sanjayatony</a
-        >, or email
-        <a href="mailto:tony@sanjaya.info" target="_blank">tony@sanjaya.info</a
-        >.
+    <div class="footer max-w-2xl mx-auto p-4 mt-10 text-center">
+      <hr />
+      <p class="text-lg mb-1">
+        Want to chat? Mention
+        <a href="https://twitter.com/sanjayatony" target="_blank">@sanjayatony</a>, or email
+        <a href="mailto:tony@sanjaya.info" target="_blank">tony@sanjaya.info</a>.
       </p>
+      <p
+        class="text-xs text-purple-600 bg-yellow-200 rounded-full py-1 px-4 inline-block"
+      >Currently I'm not looking for a new project</p>
     </div>
   </div>
 </template>
