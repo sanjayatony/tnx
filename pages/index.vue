4e272b821d24a5fa6a9455f7f94e38a1ba0cb1ca
Tony S <sanjayatony@gmail.com>
2020-07-28 15:13:46 +0700

    add new section

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
-    <p>Hey there 👋</p>
+    <p>Hey there 👋🏼</p>
     <p>Since you already here, I will introduce myself.</p>
-
     <p>
       I am Tony, a web developer and a dad,
       <n-link to="/devdad">devdad</n-link>, from Indonesia. I have been
       a professional web developer since 2012.
     </p>
-
     <p>
       Since then, I have built
       <nuxt-link to="/works">many kinds of websites</nuxt-link>, and
       focus more on WordPress. WordPress theme, plugin, maintenance, anything
       related to WordPress, you name it.
     </p>
-
     <p>
       I am currenctly working for
       <a href="https://peepso.com">PeepSo</a>, a
@@ -23,11 +20,13 @@
       effortlessly add a social network or an online community, similar to
       Facebook right inside your WordPress site.
     </p>
-
     <p>
       Besides WordPress (PHP), I also speak JS, especially Vue. But I think
       still not ready for production yet. Just wait for it. 😎
     </p>
+    <div class="py-16">
+      <h4 class="font-bold">Posts</h4>
+    </div>
   </div>
 </template>
 <script>
