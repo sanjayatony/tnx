d60f14e97f4638d8ec3eca0ac4cf77e982b9e10d
Tony S <sanjayatony@gmail.com>
2020-08-05 14:56:01 +0700

    add og and tw

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -19,7 +19,54 @@ export default {
     formatDate(date) {
       const options = { year: "numeric", month: "long", day: "numeric" };
       return new Date(date).toLocaleDateString("en", options);
-    }
-  }
+    },
+  },
+  head() {
+    return {
+      title: this.doc.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.doc.excerpt,
+        },
+        {
+          hid: "og-title",
+          name: "og:title",
+          content: this.doc.title,
+        },
+        {
+          hid: "og-description",
+          name: "og:description",
+          content: this.doc.excerpt,
+        },
+        {
+          hid: "og-image",
+          name: "og:image",
+          content: this.doc.img,
+        },
+        {
+          hid: "tw-card",
+          name: "tw:card",
+          content: "summary_large_image",
+        },
+        {
+          hid: "tw-title",
+          name: "tw:title",
+          content: this.doc.title,
+        },
+        {
+          hid: "tw-description",
+          name: "tw:description",
+          content: this.doc.excerpt,
+        },
+        {
+          hid: "tw-image",
+          name: "tw:image",
+          content: this.doc.img,
+        },
+      ],
+    };
+  },
 };
 </script>
